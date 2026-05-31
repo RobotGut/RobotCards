11ad679a63969a0d157538b09d58e39566a9b8f4
Júlio Davi <julio1704davi@gmail.com>
2026-05-31 20:54:53 -0300

    Reformulação de todas as páginas(ainda faltam detalhes), animação nos inputs do formulário de entrar e cadastrar, mudança de algumas imagens e criação do botão de menu para telas menores

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 const btnEntrar = document.querySelector(".btn-login");
 const btnCriarConta = document.querySelector(".btn-register");
 
@@ -10,109 +9,93 @@ const tituloModal = document.querySelector(".modal-box h2");
 const textoModal = document.querySelector(".modal-box p");
 
 const inputNome = document.getElementById("nome");
+const inputContainerNome = document.querySelector(
+    ".input-container:has(#nome)",
+);
 const inputEmail = document.getElementById("email");
 const inputSenha = document.getElementById("senha");
+const inputsModal = document.querySelectorAll(".input-container input");
 
+inputsModal.forEach((input) => {
+    input.addEventListener("focus", (e) => {
+        input.classList.add("focused");
+    });
 
+    input.addEventListener("blur", () => {
+        if (input.value.length == 0) input.classList.remove("focused");
+    });
+});
 
 btnEntrar.addEventListener("click", () => {
-
     modal.classList.add("active");
 
     tituloModal.innerText = "Entrar";
     textoModal.innerText = "Faça login para acessar sua conta.";
 
-    inputNome.style.display = "none";
+    inputContainerNome.style.display = "none";
 
     btnEnviar.innerText = "Entrar";
 });
 
-
-
 btnCriarConta.addEventListener("click", () => {
-
     modal.classList.add("active");
 
     tituloModal.innerText = "Criar Conta";
     textoModal.innerText = "Cadastre-se para comprar gift cards premium.";
 
-    inputNome.style.display = "block";
+    inputContainerNome.style.display = "block";
 
     btnEnviar.innerText = "Criar Conta";
 });
-
-
 
 fecharModal.addEventListener("click", () => {
     modal.classList.remove("active");
 });
 
-
 modal.addEventListener("click", (e) => {
-
     if (e.target === modal) {
         modal.classList.remove("active");
     }
-
 });
 
-
 btnEnviar.addEventListener("click", () => {
-
     const nome = inputNome.value;
     const email = inputEmail.value;
     const senha = inputSenha.value;
 
-
     if (btnEnviar.innerText === "Entrar") {
-
         if (email === "" || senha === "") {
-
             alert("Preencha todos os campos.");
             return;
-
         }
 
-        
         if (email === "admin@robotcard.com" && senha === "123456") {
-
             alert("Login realizado com sucesso!");
 
             modal.classList.remove("active");
-
         } else {
-
             alert("E-mail ou senha incorretos.");
-
         }
-
-    }
-
-  
-    else {
-
+    } else {
         if (nome === "" || email === "" || senha === "") {
-
             alert("Preencha todos os campos.");
             return;
-
         }
 
         alert("Conta criada com sucesso!");
 
         modal.classList.remove("active");
-
     }
-
 });
 
-
 function limparCampos() {
-
     inputNome.value = "";
     inputEmail.value = "";
     inputSenha.value = "";
 
+    inputsModal.forEach(inp => {
+        inp.classList.remove("focused");
+    })
 }
 
 fecharModal.addEventListener("click", limparCampos);
